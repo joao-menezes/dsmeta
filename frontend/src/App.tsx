0683dff92aefa7db24ee704e1724683512f6ee31
Joao Gabriel <60220208+joao-menezes@users.noreply.github.com>
2022-07-11 15:32:21 -0300

    Other Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
+import Header from "./Components/Header"
 import NotificationButton from "./Components/NotificationButton"
+import SalesCard from "./Components/SalesCard"
 
 function App() {
-  return(
-    <>
-      <h1>Ola Mundo</h1>
-      <NotificationButton/>
-    </>
-  )
+return(
+<>
+  <Header />
+  <main>
+    <section id="sales">
+      <div className="dsmeta-container">
+        <SalesCard />
+      </div> 
+    </section>
+  </main>
+</>
+)
 
 }
 
